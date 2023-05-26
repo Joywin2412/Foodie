d36e95a40d192b2929d2460a6b3bc5227355d899
LighteningBolt13 <joywinpinto1@gmail.com>
2023-05-26 18:09:52 +0530

    useRoutes() fixed and also RTL library testing check

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 // require("dotenv").config();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 // process.env.REACT_APP_API_KEY = "123";
